450d86fe2f9226bb9460a81377d7b5f73dc01163
Mitra Sarvaiya <137141793+MitraSarvaiya@users.noreply.github.com>
2024-06-25 20:50:00 +0530

    Add files via upload

---
+++ Module5/Q5.py
@@ -0,0 +1,9 @@
+#Q5 What is a QuerySet?Write program to create a new Post object in database
+
+"""A QuerySet is a collection of data from a database.
+
+A QuerySet is built up as a list of objects.
+
+QuerySets makes it easier to get the data you actually need, by allowing you to filter and order the data at an early stage."""
+
+
